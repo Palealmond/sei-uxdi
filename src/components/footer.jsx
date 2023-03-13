fead21972432e48b3bc41a7972372138b6e09f7e
Kenny <knnyshn@gmail.com>
2023-03-13 16:31:13 -0400

    footer work

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
+      <div className="footer-container">
         {/* <img src= /> */}
-        <span className="text-muted">Footer</span>
+        <h4>About Us</h4>
+        <h4>Careers</h4>
+        <h4>Privacy Notice</h4>
+        <h4>Social Impact</h4>
+        <h4>Order and Pickup</h4>
+        <h4>© 2023 Bagel Meets Coffee</h4>
       </div>
     </footer>
   );
 }
+
+export default Footer;
