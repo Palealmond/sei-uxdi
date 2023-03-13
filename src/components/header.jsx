1c3e536fbc842f03876d83398d9de4d961b1d2fd
Basam Hassan <basammoses@gmail.com>
2023-03-13 14:50:34 -0400

    basam- boiler for footer and header/also added some JSON files to plug into backend server

---
+++ src/components/header.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react"
+
+const Header = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
+  function timeOfDay() { 
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours < 12) {
+      return "morning";
+    } else if (hours < 18) {
+      return "afternoon";
+    } else {
+      return "evening";
+    }
+  }
+
+
+
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", isDark);
+  }, [isDark]);
+
+
+  useEffect(() => {
+    timeOfDay();
+  }, []);
+
+  
+
+  return (
+    <header className="header">
+      <div className="container">
+        <button className="btn btn-dark" onClick={toggleTheme}>
+          {isDark ? "Light" : "Dark"}
+        </button>
+        <h1>Good {timeOfDay()}!</h1>
+        
+        <span className="text-muted">Header</span>
+      </div>
+    </header>
+  );
+}
