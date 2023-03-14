1ae784efa5e664711cbcafa39a7be15e50fd31a5
Basam Hassan <basammoses@gmail.com>
2023-03-14 09:46:58 -0400

    scoller prototype

--- src/components/foodHandler.js
+++ src/components/foodHandler.js
@@ -2,25 +2,26 @@
 import foodData from "./food.json"
 import cofeeData from "./coffee.json"
 import React from "react";
+import "./horizontalScroll.css";
 
 
 export function FoodHandler() {
   return (
-    <div className="media-group"> 
+    <>
     {
       
       foodData.map((food) => (
           <div className="media-element">
             <img src={food.img} alt="" />
-            <h3>{food.productName}</h3>
-            <p>{food.description}</p>
+            <h3 className="fooditem">{food.productName}</h3>
+            <p className="fooddescription">{food.description}</p>
           </div>
         
       ))
     
     
       }
-    </div>
+    </>
   );
 }
 
@@ -30,7 +31,7 @@ export function FoodHandler() {
 
 export function CoffeeHandler() {
   return (
-    <div className="media-group"> 
+   <>
     {
       cofeeData.map((coffee) => (
           <div className="media-element">
@@ -41,6 +42,6 @@ export function CoffeeHandler() {
         
       ))
       }
-    </div>
+    </>
   );
 }
