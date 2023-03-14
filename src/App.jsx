aee3ad14e2209330a4326d525d11e1180229cc46
Kenny <knnyshn@gmail.com>
2023-03-13 21:49:13 -0400

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/header'
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
+      <Footer />
     </div>
   );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import logo from '../assets/logo.svg'
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        {/* <img src= /> */}
+        <img className="footer-image" src={logo}/>
         <h4>About Us</h4>
         <h4>Careers</h4>
         <h4>Privacy Notice</h4>
         <h4>Social Impact</h4>
         <h4>Order and Pickup</h4>
-        <h4>© 2023 Bagel Meets Coffee</h4>
+        <h5>© 2023 Bagel Meets Coffee</h5>
       </div>
     </footer>
   );
