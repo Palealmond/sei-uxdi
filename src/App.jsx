89958dd7ba0b06aadac6c8076f3d2a53465500d9
Basam Hassan <basammoses@gmail.com>
2023-03-14 14:22:14 -0400

    pro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 import Header from './components/header'
 import Footer from './components/footer';
+import { CarSlider } from './components/carslider';
+import HorizontalScrollBar from './components/horizontalScroll';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CarSlider />
+      <HorizontalScrollBar />
+
       <Footer />
     </div>
   );
